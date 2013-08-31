7e54f26bacd5b00ca119731974a2f049f263e48a
Luke-Michael-Brown <Luke.Michael.Brown@gmail.com>
2013-08-31 13:04:58 -0400

    small changes to PuzzleStorage Module

--- web/js/PuzzleStorage.js
+++ web/js/PuzzleStorage.js
@@ -1,38 +1,38 @@
 //level: [line] [puzzle] -> level
 //A level contains the lines for the graph and an array of puzzle soluntions. Which contain the correct lines,  
 //the neccecary rotation and then the details containing strings for the description and hints
-function level(graph, puzzles) {
+function Level(graph, puzzles) {
     "use strict";
-    return {"getGraph" : graph, "getPuzzles" : puzzles};
+    return {"graph" : graph, "puzzles" : puzzles};
 }
 
 
 //puzzle: solution details -> puzzle
-function puzzle(soln, dets) {
+function Puzzle(soln, dets) {
     "use strict";
-    return {"getSolution" : soln, "getDetails" : dets};
+    return {"solution" : soln, "details" : dets};
 }
 
 //solution: number [line] -> solution
-function solution(roation, lines) {
+function Solution(roation, lines) {
     "use strict";
-    return {"getSolutionRoation" : roation, "getSolutionLines" : lines};
+    return {"solutionRoation" : roation, "solutionLines" : lines};
 }
 
 //details: String String String String -> details
-function details(description, hint1, hint2, hint3) {
+function Details(description, hint1, hint2, hint3) {
     "use strict";
-    return {"getDescription" : description, "getHint1" : hint1, "getHint2" : hint2, "getHint3" : hint3};
+    return {"description" : description, "hint1" : hint1, "hint2" : hint2, "hint3" : hint3};
 }
 
 //line: posn posn -> line
-function line(p1, p2) {
+function Line(p1, p2) {
     "use strict";
     return {"p1" : p1, "p2" : p2};
 }
 
 //posn: number number -> posn
-function posn(x, y) {
+function Posn(c, l) {
     "use strict";
-    return {"x" : x, "y" : y};
+    return {"x" : c, "y" : l};
 }

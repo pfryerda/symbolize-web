e9cb18e3479fbca7aab9ed3fd5dd6d258412a895
pfryerda <pfryerdavis@gmail.com>
2013-09-13 00:20:16 -0400

    Changes to Clear and Reset buttons
    Now present dialogs.  Pressing "Quit" takes you to level selector.

--- web/js/main.js
+++ web/js/main.js
@@ -6,6 +6,36 @@ App.populator('home', function (page) {
 
 App.populator('game', function (page) {
 	console.log("loaded game");
+
+	$(page).find('.clear').on('click', function () {
+		console.log("clear");
+		App.dialog({
+				title        : "Quit Level?",
+				text         : "This will not save your progress so far.  Are you sure you want to do this?",
+				quitButton   : "Quit",
+				cancelButton : "Cancel"
+			}, function (result) {
+
+			if (result ===  "quit") {
+				App.load("levels")
+			}
+		});
+    });
+
+    $(page).find('.reset').on('click', function () {
+		console.log("reset");
+		App.dialog({
+				title        : "Quit Level?",
+				text         : "This will not save your progress so far.  Are you sure you want to do this?",
+				quitButton   : "Quit",
+				cancelButton : "Cancel"
+			}, function (result) { //result is a string
+
+			if (result ===  "quit") {
+				App.load("levels")
+			}
+		});
+    });
 });
 
 App.populator('levels', function (page) {

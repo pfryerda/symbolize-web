0b3e03b104e44f7249a8b9124f8ff6f6b13f901e
Luke-Michael-Brown <Luke.Michael.Brown@gmail.com>
2013-09-19 10:27:39 -0400

    fixed bug where you could not erase your own lines when you were out of erases

--- web/js/events.js
+++ web/js/events.js
@@ -49,29 +49,27 @@ function addLine(l, c, ctx) {
 //removeLine: Line -> Canvas -> Context -> Void
 function removeLine(line, c, ctx) {
     "use strict";
-    if (currSoln.linesErased < (currLevel.restriction.erase)) {
-        var eraseLine = [-1, new Line(new Posn(0, 0), new Posn(100, 100), "App")];
-        for(var i = 0; i < currSoln.solution.length; i+= 1) {
-            if (interset(currSoln.solution[i], line)) {
-                if (lineEqual(currSoln.solution[i], line) || 
-                    (lineToPointDistance(currSoln.solution[i], line.p1) < lineToPointDistance(eraseLine[1], line.p1)) ||
-                        (lineToPointDistance(currSoln.solution[i], line.p1) === lineToPointDistance(eraseLine[1], line.p1) && 
-                            lineLength(currSoln.solution[i]) > lineLength(eraseLine[1]))) {
-                    eraseLine = [i, currSoln.solution[i]];
-                }
+    var eraseLine = [-1, new Line(new Posn(0, 0), new Posn(100, 100), "App")];
+    for(var i = 0; i < currSoln.solution.length; i+= 1) {
+        if (interset(currSoln.solution[i], line)) {
+            if (lineEqual(currSoln.solution[i], line) || 
+                (lineToPointDistance(currSoln.solution[i], line.p1) < lineToPointDistance(eraseLine[1], line.p1)) ||
+                    (lineToPointDistance(currSoln.solution[i], line.p1) === lineToPointDistance(eraseLine[1], line.p1) && 
+                        lineLength(currSoln.solution[i]) > lineLength(eraseLine[1]))) {
+                eraseLine = [i, currSoln.solution[i]];
             }
         }
+    }
 
-        if (eraseLine[0] > -1) {
-            console.log("removing line from solution");
-            currSoln.solution.splice(eraseLine[0], 1);
-            if (eraseLine[1].owner === "App") { currSoln.linesErased += 1; }
-            else { currSoln.linesDrawn -=1; }
-            currSoln.moves.unshift(["erase", new Line(new Posn(eraseLine[1].p1.x, eraseLine[1].p1.y), 
-                new Posn(eraseLine[1].p2.x, eraseLine[1].p2.y), eraseLine[1].owner)]);
-            drawSolution(currSoln, c, ctx);
-            console.log("removed line from solution");
-        }
+    if (eraseLine[0] > -1 && (eraseLine[1].owner === "User" || currSoln.linesErased < (currLevel.restriction.erase))) {
+        console.log("removing line from solution");
+        currSoln.solution.splice(eraseLine[0], 1);
+        if (eraseLine[1].owner === "App") { currSoln.linesErased += 1; }
+        else { currSoln.linesDrawn -=1; }
+        currSoln.moves.unshift(["erase", new Line(new Posn(eraseLine[1].p1.x, eraseLine[1].p1.y), 
+            new Posn(eraseLine[1].p2.x, eraseLine[1].p2.y), eraseLine[1].owner)]);
+        drawSolution(currSoln, c, ctx);
+        console.log("removed line from solution");
     } else {
         var options = {
             text: "Cannot erase any more lines",  // String
